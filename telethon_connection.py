b51ec97f9a0a79fa8fb5109a3d4d9f01695491e7
SoenkeTaod <soenke.maibach@taod.de>
2022-02-02 10:14:51 +0100

    Joining multiple groups based on url list works

--- telethon_connection.py
+++ telethon_connection.py
@@ -6,21 +6,32 @@ from telethon.tl.functions.messages import ImportChatInviteRequest, GetHistoryRe
 api_id = 14360095
 api_hash = '4411841d81d92fa47f4052a5ad9a1dd0'
 
-# create client
-client = TelegramClient('mike2', api_id, api_hash)
-client.start()
-
-# join a public channel via the invite link
-from telethon.tl.functions.channels import JoinChannelRequest
-channel = 'https://t.me/BinanceWaves'
-client(JoinChannelRequest(channel))
-
-## loop is done, we're in all channels
 testing = True
 if testing:
     limit = 5
 else:
     limit=None
+
+# create client
+client = TelegramClient('m', api_id, api_hash)
+client.start()
+
+# join a public channels via the invite link
+# load join urls
+import pickle
+path = 'data/tele.pickle'
+with open(path, 'rb') as f:
+    channel_list = pickle.load(f)   
+
+from telethon.tl.functions.channels import JoinChannelRequest
+for channel in channel_list[:5]:
+    try:
+        client(JoinChannelRequest(channel))
+    except Exception as e:
+        print(e)
+        # continue
+## loop is done, we're in all channels
+
 # access all open dialogs
 dialogs = client.get_dialogs(limit=limit)
 # get chats
@@ -39,7 +50,7 @@ msg_dict = {}
 # It's ok in this instance because this is supposed to run a single time
 # and it is a case of asynchronous programming 
 for chat in chat_list:
-    for message in client.iter_messages(chat,limit=5):
+    for message in client.iter_messages(chat,limit=limit):
         try: 
             text = message.text.replace(',','')
         except:
@@ -60,7 +71,7 @@ for chat in chat_list:
     print(chat)
 import pandas as pd
 msg_df = pd.DataFrame.from_dict(msg_dict,orient='index') 
-print(msg_df)
+# print(msg_df)
     # print(message.sender_id, ':', message.text,':',message.date)
 msg_df.to_csv('data/raw/messages.csv')
 

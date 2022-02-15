b636a00f38b7ab165703f3adff24d1dd97a84f9e
SoenkeTaod <soenke.maibach@taod.de>
2022-02-15 16:11:34 +0100

    reverted back to the old version, new one was buggy

--- telethon_connection.py
+++ telethon_connection.py
@@ -1,11 +1,7 @@
-#when following ERROR occures:
-#Cannot find any entity corresponding to "Altcoin Alerts Community"
-#look here: https://docs.telethon.dev/en/latest/concepts/entities.html#summary
 
 # imports
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import ImportChatInviteRequest, GetHistoryRequest
-import pandas as pd
 
 api_id = 14360095
 api_hash = '4411841d81d92fa47f4052a5ad9a1dd0'
@@ -17,7 +13,7 @@ else:
     limit=None
 
 # create client
-client = TelegramClient('1202a', api_id, api_hash)
+client = TelegramClient('0602b', api_id, api_hash)
 client.start()
 
 # join a public channels via the invite link
@@ -25,119 +21,37 @@ client.start()
 import pickle
 path = 'data/tele.pickle'
 with open(path, 'rb') as f:
-    channel_list = pickle.load(f)
-    f.close()   
+    channel_list = pickle.load(f)   
+batch_number = 0
+batch = channel_list[80*batch_number:80*(batch_number+1)]
 
 from telethon.tl.functions.channels import JoinChannelRequest
-from telethon.tl.functions.channels import LeaveChannelRequest
-
-#divivde in batches
-def divide_batch(l,n):          
-    # looping till length l
-    for i in range(0, len(l), n): 
-        yield l[i:i + n]
-
-#leere list für alle batch ergebnisse
-msg_df=[]
-
-#batch size
-n=80
-batches = list(divide_batch(channel_list, n))
-
-# create a batch id
-batch_id = 0
-
-#iterate through first batch
-for channels in batches[:4]:
-    batch_id = batch_id+1
-    for i in channels:
-        try:
-            client(JoinChannelRequest(i))
-        except Exception as e:
-            print(e)
-            continue
-    ## loop is done, we're in all channels
-
-    # access all open dialogs
-    dialogs = client.get_dialogs(limit=limit)
-    # get chats
-    chat_list = [
-        dialog.name
-        for dialog in dialogs
-    ]
-
-    from datetime import datetime as dt
-
-    # instantiate empty dict to collect all messages
-    msg_dict = {}
-
-
-    #using a list comprehension to iterate over all the chats
+for channel in batch:
     try:
-        list_comp=[message for chat in chat_list for message in client.iter_messages(chat,limit=limit)]
-    except:
-        ValueError
-    #parameter die helfen durch die list_comp zu iterarieren
-    end=limit
-    start=0
+        client(JoinChannelRequest(channel))
+    except Exception as e:
+        print(e)
+        # continue
+## loop is done, we're in all channels
 
-    #die zugehörigen chats zu den messages zuordnen
-    for chat in chat_list:
-        for j in range(start,end):
-            try: 
-                text = str(
-                    list_comp[j].text
-                    .replace(',','')
-                    .replace("'",'')
-                    .replace('**','')
-                    .replace('\n','.')
-                    .replace('##','')
-                    .lower()
-                )
-            except:
-                text = ''
-                end=end-limit+1 #damit leere strings übersprungen werden
-                start=j-limit+1
-                break
-            date = list_comp[j].date
-            id = str(list_comp[j].sender_id)
-            message_id = str(list_comp[j].id)+'_'+chat+'_'+date.strftime('%Y-%m-%d')
-            update = {
-                message_id:{
-                    'text':text,
-                    'date':date,
-                    'id':id,
-                    'chat':chat
-                }
-            }
-            msg_dict.update(update)
-            msg=list_comp[j]
-        #print (chat) #for debug
-        end=end+limit
-        start=start+limit
+# access all open dialogs
+dialogs = client.get_dialogs(limit=limit)
+# get chats
+chat_list = [
+    dialog.name
+    for dialog in dialogs
+]
 
-    msg_df = pd.DataFrame.from_dict(msg_dict,orient='index') 
-    msg_df.to_csv('data/chats/messages_batch{}.csv'.format(str(batch_id)), mode='a')
+from datetime import datetime as dt
 
-    #leaving all channels
-    for i in channels:
-        try:
-            client(LeaveChannelRequest(i))
-        except Exception as e:
-            print(e)
-            continue
-    
-    #print(msg_df)
-        # print(message.sender_id, ':', message.text,':',message.date)
-    
+# instantiate empty dict to collect all messages
+msg_dict = {}
 
-# send me an update message
-client.send_message('me', 'Last task done')
-
-
-#_______________________________________________________
-#the old code without list comprehension
-"""for chat in chat_list:
+# iterate over all the chats and messages to save them
+# this normally is BAD PRACTICE! There are better ways than using loops. 
+# It's ok in this instance because this is supposed to run a single time
+# and it is a case of asynchronous programming 
+for chat in chat_list:
     for message in client.iter_messages(chat,limit=limit):
         try: 
             text = str(
@@ -151,7 +65,6 @@ client.send_message('me', 'Last task done')
             )
         except:
             text=''
-            
         date = message.date
         id = str(message.sender_id)
         message_id = str(message.id)+'_'+chat+'_'+date.strftime('%Y-%m-%d')
@@ -165,4 +78,22 @@ client.send_message('me', 'Last task done')
         }
         msg_dict.update(update)
         msg=message
-    print(chat)"""
+    print(chat)
+
+# leave the channels
+from telethon.tl.functions.channels import LeaveChannelRequest
+for channel in batch:
+    try:
+        client(LeaveChannelRequest(channel))
+    except Exception as e:
+        print(e)
+
+import pandas as pd
+msg_df = pd.DataFrame.from_dict(msg_dict,orient='index') 
+# print(msg_df)
+    # print(message.sender_id, ':', message.text,':',message.date)
+msg_df.to_csv('data/raw/messages_batch2_old.csv')
+
+
+# send me an update message
+client.send_message('MikeTelethon', 'Last task done'+str(len(batch)))
